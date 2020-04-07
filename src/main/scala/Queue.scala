29cdb8092db01e851ad2fe17c68565570eca6735
Jitlada Supreeyaporn <jitlada.supreeyaporn@agoda.com>
2020-04-07 23:22:19 +0700

    practicing about traits and implement to class

--- src/main/scala/Queue.scala
+++ src/main/scala/Queue.scala
@@ -1,3 +1,19 @@
-object Queue {
+trait QueueGeneric {
+  def put(str: String): Array[String]
+  def get(): String
+}
 
+class Queue extends QueueGeneric {
+  var arr: Array[String] = Array()
+
+  def put(str: String): Array[String] = {
+    arr = arr.appended(str)
+    arr
+  }
+
+  def get(): String = {
+    val head = arr.head
+    arr = arr.drop(1)
+    head
+  }
 }
--- src/test/scala/QueueTest.scala
+++ src/test/scala/QueueTest.scala
@@ -1,3 +1,13 @@
-class QueueTest {
+import org.scalatest._
 
+class QueueTest extends FunSuite{
+  val q = new Queue
+
+  test("q.put(hello) will return Array(hello)"){
+    assert(q.put("hello").sameElements(Array("hello")))
+  }
+
+  test("q.put(world) will return Array(hello, world)"){
+    assert(q.put("world").sameElements(Array("hello", "world")))
+  }
 }
