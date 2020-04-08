62f11a65a103cc0ad93908ebb99444872cc0a3df
Jitlada Supreeyaporn <jitlada.supreeyaporn@agoda.com>
2020-04-08 15:00:31 +0700

    submit exercise 3

--- src/main/scala/DoubleComparator.scala
+++ src/main/scala/DoubleComparator.scala
@@ -1,3 +1,6 @@
-object DoubleComparator {
-
+object DoubleComparator extends App {
+  def ~=(x:Double, y:Double)(implicit diff:Double = 0.5): Boolean = {
+    if((x-y).abs <= diff) true
+    else false
+  }
 }
--- src/test/scala/DoubleComparatorTest.scala
+++ src/test/scala/DoubleComparatorTest.scala
@@ -1,3 +1,27 @@
-class DoubleComparatorTest {
+import org.scalatest.FunSuite
 
+class DoubleComparatorTest extends FunSuite{
+  test("abs(x-y) > 0.5(default) will be false"){
+    assert(DoubleComparator.~=(1,2)===false)
+  }
+
+  test("abs(x-y) = 0.5(default) will be true"){
+    assert(DoubleComparator.~=(1,1.5)===true)
+  }
+
+  test("abs(x-y) < 0.5(default) will be true"){
+    assert(DoubleComparator.~=(1,1.3)===true)
+  }
+
+  test("abs(x-y) > 1(param) will be false"){
+    assert(DoubleComparator.~=(1,3)(1)===false)
+  }
+
+  test("abs(x-y) = 1(param) will be true"){
+    assert(DoubleComparator.~=(1,2)(1)===true)
+  }
+
+  test("abs(x-y) < 1(param) will be true"){
+    assert(DoubleComparator.~=(1,1.3)(1)===true)
+  }
 }
