5473dc6392c794bacf631c1985a4a429d97da60b
Jitlada Supreeyaporn <jitlada.supreeyaporn@agoda.com>
2020-04-23 15:30:42 +0700

    add neg num testcase for exercise 4

--- src/test/scala/GCDTest.scala
+++ src/test/scala/GCDTest.scala
@@ -14,15 +14,27 @@ class GCDTest extends FunSuite{
     assert(GCD.gcd(3,3)===3)
   }
 
-  test("gcd(x,y) iff x%y==0 and x>=y return y"){
+  test("gcd(x,y) iff x%y==0 and x>=y and x,y>0 return y"){
     assert(GCD.gcd(10,5)===5)
   }
 
-  test("gcd(x,y) iff x%y==0 and x<y return x"){
+  test("gcd(x,y) iff x%y==0 and x<y and x,y>0 return x"){
     assert(GCD.gcd(5,10)===5)
   }
 
   test("gcd(x,y) iff x%y!=0 and x,y are prime num return 1"){
     assert(GCD.gcd(5,3)===1)
+  }
+
+  test("gcd(x,y) iff x,y<0 and abs(x), abs(y) are prime number return -1"){
+    assert(GCD.gcd(-5,-3)===(-1))
+  }
+
+  test("gcd(x,y) iff x%y==0 and x>=y and x,y<0 return x"){
+    assert(GCD.gcd(-2,-4)===(-2))
+  }
+
+  test("gcd(x,y) iff x%y==0 and x<y and x,y<0 return y"){
+    assert(GCD.gcd(-4,-2)===(-2))
   }
 }
