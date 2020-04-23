db9539daeb915bf00810a45ac0949fc6c44080c6
Jitlada Supreeyaporn <jitlada.supreeyaporn@agoda.com>
2020-04-23 16:49:42 +0700

    edit exercise 11

--- src/main/scala/FindNotNone.scala
+++ src/main/scala/FindNotNone.scala
@@ -1,24 +1,18 @@
 object FindNotNone extends App {
   def firstNotNone(x: Option[Any],y: Option[Any],z: Option[Any]): Any =
-    (x,y,z) match {
-      case(None, None, None) => None
-      case (None, None, z) => z
-      case (None, y, _) => y
-      case (x,_,_) => x
-    }
+    if (x.isDefined) x
+    else if (y.isDefined) y
+    else z
 
   def productFromFirstNotNone(x: Option[Any],y: Option[Any],z: Option[Any]): Any =
-    (x,y,z) match {
-      case (None, None, None) => None
-      case (Some(x), _, _) => x
-      case (None, Some(y), _) => y
-      case (None, None, Some(z)) => z
-    }
+    if (x.isDefined) x.get
+    else if (y.isDefined) y.get
+    else z.getOrElse(None)
 
   def firstItemInListThatNotNone(list: List[Option[Any]]): Option[Any] = list match {
     case List() => None
     case l :: list => l match {
-      case None => if (list.dropRight(list.length-2).isEmpty) None else firstItemInListThatNotNone(list.dropRight(list.length-2))
+      case None => if (list.isEmpty) None else firstItemInListThatNotNone(list)
       case l => l
     }
   }
