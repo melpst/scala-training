a81892a94bee5f8c9bd6d9a30bff6d77f70ccf52
Jitlada Supreeyaporn <jitlada.supreeyaporn@agoda.com>
2020-04-24 16:19:22 +0700

    edit exercise 6

--- src/main/scala/StringMethods.scala
+++ src/main/scala/StringMethods.scala
@@ -1,17 +1,19 @@
 object StringMethods {
 
-  def toLower(str: String): Array[String] = str.toLowerCase.split(" ")
+  implicit class methods(str: String) {
+    private def toLower(s: String): Array[String] = s.toLowerCase.split(" ")
 
-  def longestWord(str: String): String = toLower(str).maxBy(s => s.length)
+    def longestWord: String = toLower(str).maxBy(s => s.length)
 
-  def mostCommonWord(str: String): String = toLower(str).maxBy(s => toLower(str).count(a => a.equals(s)))
+    def mostCommonWord: String = toLower(str).maxBy(s => toLower(str).count(a => a.equals(s)))
 
-  def mostCommonLetter(str: String): Option[Char] =
-    if (str.isEmpty) None
-    else Some(toLower(str).map(s => s.toList).flatten
-    .maxBy(c => toLower(str).map(s => s.toList).flatten.count(l => l.equals(c))))
+    def mostCommonLetter: Option[Char] =
+      if (str.isEmpty) None
+      else Some(toLower(str).map(s => s.toList).flatten
+        .maxBy(c => toLower(str).map(s => s.toList).flatten.count(l => l.equals(c))))
 
-  def mapCharToWords(str: String): Map[Char, Array[String]] =
-    toLower(str).map(s => s.toList.map(c => (c, s))).flatten.groupBy(t => t._1)
-    .transform((k, v) => v.map(x => x._2).distinct)
+    def mapCharToWords: Map[Char, Array[String]] =
+      toLower(str).map(s => s.toList.map(c => (c, s))).flatten.groupBy(t => t._1)
+        .transform((k, v) => v.map(x => x._2).distinct)
+  }
 }
